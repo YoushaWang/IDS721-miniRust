ef1a29fdc8a81af982eab9abe035bc55f8ed4e38
YoushaWang <122926209+YoushaWang@users.noreply.github.com>
2023-02-05 04:02:40 +0000

    add miniRust4

---
+++ miniRust4/target/debug/deps/hello-36b236bb53296e30.d
@@ -0,0 +1,5 @@
+/workspaces/IDS721/miniRust4/target/debug/deps/hello-36b236bb53296e30: src/main.rs
+
+/workspaces/IDS721/miniRust4/target/debug/deps/hello-36b236bb53296e30.d: src/main.rs
+
+src/main.rs:
---
+++ miniRust4/target/debug/libhello.d
@@ -0,0 +1,1 @@
+/workspaces/IDS721/miniRust4/target/debug/libhello.rlib: /workspaces/IDS721/miniRust4/src/lib.rs
