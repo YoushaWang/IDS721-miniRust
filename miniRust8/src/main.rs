423cbb54a7a9a879fc6671c6c13fc9957da25c6b
YoushaWang <122926209+YoushaWang@users.noreply.github.com>
2023-03-18 23:08:26 +0000

    add proj 8

--- miniRust8/src/main.rs
+++ miniRust8/src/main.rs
@@ -1,59 +1,40 @@
+use rand::Rng;
 use std::io::{self, Write};
-use std::thread::sleep;
-use std::time::{Duration};
-// use chrono::{DateTime, Local, TimeZone};
 
 fn main() {
-    println!("Welcome to the Rust Digital Clock and Timer!");
-
     loop {
-        println!("Please select an option:");
-        println!("1. Digital Clock");
-        println!("2. Timer");
-        println!("3. Quit");
+        let fortune = generate_fortune();
+        println!("{}", fortune);
 
-        let mut option = String::new();
-        io::stdin().read_line(&mut option).expect("Failed to read line");
-
-        match option.trim() {
-            "1" => digital_clock(),
-            "2" => timer(),
-            "3" => break,
-            _ => println!("Invalid option!"),
-        }
-    }
-
-    println!("Thanks for using the Rust Digital Clock and Timer!");
-}
-
-fn digital_clock() {
-    loop {
-
-        let current_time = chrono::Local::now().with_timezone(&chrono_tz::America::New_York).format("%H:%M:%S");
-        println!("\r{}", current_time);
+        print!("Press Enter to generate a new message, or type 'quit' to exit...");
         io::stdout().flush().unwrap();
-        sleep(Duration::from_secs(1));
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        if input.trim() == "quit" {
+            break;
+        }
     }
 }
 
-fn timer() {
-    println!("Please enter the duration of the timer in seconds:");
-    let mut duration_str = String::new();
-    io::stdin().read_line(&mut duration_str).expect("Failed to read line");
+fn generate_fortune() -> String {
+    const FORTUNES: &[&str] = &[
+        "A bird in the hand is worth two in the bush.",
+        "A penny saved is a penny earned.",
+        "All work and no play makes Jack a dull boy.",
+        "Don't count your chickens before they're hatched.",
+        "Every dog has its day.",
+        "Every cloud has a silver lining.",
+        "If you can't stand the heat, get out of the kitchen.",
+        "It takes two to tango.",
+        "Life is like a box of chocolates; you never know what you're gonna get.",
+        "Practice makes perfect.",
+        "When in Rome, do as the Romans do.",
+        "You can't judge a book by its cover.",
+    ];
 
-    let duration: u64 = match duration_str.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Invalid duration! Defaulting to 10 seconds.");
-            10
-        }
-    };
-
-    for i in (0..duration).rev() {
-        print!("\r{} ", i);
-        io::stdout().flush().unwrap();
-        sleep(Duration::from_secs(1));
-    }
-
-    println!("\rTimer complete!");
+    let mut rng = rand::thread_rng();
+    let index = rng.gen_range(0..FORTUNES.len());
+    FORTUNES[index].to_owned()
 }
